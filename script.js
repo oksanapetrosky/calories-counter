88fec192f36117c7aea7cc1a6652194d50f59f7d
Douglas Petrosky <douglaspetrosky@Douglass-MacBook-Pro.local>
2024-12-15 17:53:05 -0500

    Declare cleanInputString function and declare regex variable

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
+
+
+function cleanInputString(str) {
+    const regex = /\+-/;
+  }
